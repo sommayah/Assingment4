d08abfb277ef2d7d4bda7899cb5f04f722671022
Sommayah Soliman <arabic.applications@gmail.com>
2015-12-14 16:35:18 -0800

    first version, android and java libraries added

---
+++ jokedisplay/src/main/java/com/example/sommayahsoliman/jokedisplay/JokeActivityFragment.java
@@ -0,0 +1,31 @@
+package com.example.sommayahsoliman.jokedisplay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class JokeActivityFragment extends Fragment {
+
+    public JokeActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_joke, container, false);
+        Intent intent = getActivity().getIntent();
+        String joke = intent.getStringExtra(JokeActivity.JOKE_KEY);
+        TextView jokeTextView = (TextView) rootView.findViewById(R.id.jokeText);
+        if (joke != null && joke.length() != 0) {
+            jokeTextView.setText(joke);
+        }
+        return rootView;
+    }
+}
