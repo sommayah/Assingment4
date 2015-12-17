d6b746c302e121fcf43451ad6095a128608c8b97
Sommayah Soliman <arabic.applications@gmail.com>
2015-12-16 19:17:46 -0800

    added tests

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/TaskAndroidTest.java
@@ -0,0 +1,24 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.InstrumentationTestCase;
+import android.text.TextUtils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sommayahsoliman on 12/16/15.
+ */
+public class TaskAndroidTest extends InstrumentationTestCase{
+    public void testAsyncTask() throws Exception{
+        try {
+            EndpointsAsyncTask jokeTask = new EndpointsAsyncTask();
+            jokeTask.execute(getInstrumentation().getContext());
+            String joke = jokeTask.get(30, TimeUnit.SECONDS);
+            assertFalse(TextUtils.isEmpty(joke));
+        } catch (Exception e){
+            fail("Timed out");
+        }
+    }
+
+
+}
