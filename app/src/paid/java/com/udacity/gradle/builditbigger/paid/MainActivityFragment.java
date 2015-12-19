f5527a00a8b4064c2ac61b0f5dda90191fd9fcbb
Sommayah Soliman <arabic.applications@gmail.com>
2015-12-19 12:26:24 -0800

    fixed some issues

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+
+import com.udacity.gradle.builditbigger.R;
 
 
 /**
