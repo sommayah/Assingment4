bd512949a5a996871d1c9f3eee0faf27f1a453f5
Sommayah Soliman <arabic.applications@gmail.com>
2015-12-18 18:03:18 -0800

    fixed some issues

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -1,16 +1,14 @@
-package com.udacity.gradle.builditbigger;
+package com.udacity.gradle.builditbigger.free;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-
+import com.udacity.gradle.builditbigger.R;
 
 /**
  * A placeholder fragment containing a simple view.
